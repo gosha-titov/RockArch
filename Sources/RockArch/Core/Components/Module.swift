5db809335b3b3d5b206531f4013999c3584fd7db
gosha <gosha.titov@icloud.com>
2023-01-10 01:26:56 +0300

    Feat: add the `Lifecycle` methods; define the `RAEmptyModule` class.

--- Sources/RockArch/Core/Components/Module.swift
+++ Sources/RockArch/Core/Components/Module.swift
@@ -13,15 +13,120 @@ open class RAModule: RAComponent {
     /// The current state of this module.
     public private(set) var state: RAComponentState = .inactive
     
-    /// A logger that logs messages.
-    public internal(set) var logger: RALogger?
+    /// A boolean value that indicates whether this modue is active.
+    public final var isActive: Bool { state == .active }
+    
+    /// A boolean value that indicates whether this modue is inactive.
+    public final var isInactive: Bool { state == .inactive }
+    
+    /// A boolean value that indicates whether this modue is suspended.
+    public final var isSuspended: Bool { state == .suspended }
+    
+    /// A boolean value that indicates whether this module is loaded into the parent memory.
+    public private(set) var isLoaded: Bool = false
     
     
-    // MARK: - Public Init
+    // MARK: - Internal Components
     
-    /// Creates a named module.
-    public init(name: String) {
+    /// The internal interactor of this module.
+    internal let interactor: RAAbstractInteractor
+    
+    /// The internal router of this module.
+    internal let router: RARouter
+    
+    /// The internal view of this module, or `nil`.
+    internal let view: RAAbstractView?
+    
+    /// The internal builder of this module, or `nil`.
+    internal let builder: RABuilder?
+    
+    
+    // MARK: - Lifecycle
+    
+    /// Called when a parent module loads this module into its memory.
+    internal final func load() -> Void {
+        defer { log("loaded into memory", category: "ModuleLifecycle") }
+        isLoaded = true
+    }
+    
+    /// Called when a parent module starts this module.
+    internal final func start() -> Void {
+        defer { log("started working", category: "ModuleLifecycle") }
+        state = .active
+    }
+    
+    /// Called when this module starts a child module.
+    internal final func suspend() -> Void {
+        defer { log("suspended working", category: "ModuleLifecycle") }
+        state = .suspended
+    }
+    
+    /// Called when a child module stops its work.
+    internal final func resume() -> Void {
+        defer { log("resumed working", category: "ModuleLifecycle") }
+        state = .active
+    }
+    
+    /// Called when this module should stop its work for some reason.
+    internal final func stop() -> Void {
+        defer { log("stopped working", category: "ModuleLifecycle") }
+        state = .inactive
+    }
+    
+    /// Called when a parent module unloads this module from its memory.
+    internal final func unload() -> Void {
+        defer { log("unloaded from memory", category: "ModuleLifecycle") }
+        isLoaded = false
+    }
+    
+    
+    // MARK: - Init and Deinit
+    
+    /// Creates a named module with the given components.
+    ///
+    /// - Parameter name: A string that will be associated with this module.
+    /// The inner components (interactor, router, view, builder) of this module will also have this name.
+    /// It will be displayed in logs, paths, links, etc.
+    ///
+    /// - Parameter interactor: The interactor that is responsible for all business logic of this module.
+    /// It's also a built-in delegate of the module lifecycle and It can interact with other related interactors.
+    /// Implement this by subclassing the `RAInteractor` class.
+    ///
+    /// - Parameter router: The router that is responsible for the hierarchy of modules.
+    /// It can show and hide child modules, and it can complete this module.
+    /// Implement this by subclassing the `RARouter` class.
+    ///
+    /// - Parameter view: The view that is responsible for configurating and updating UI, catching and handling user interactions.
+    /// Implement this by subclassing the `RAView` class.
+    /// If this is a logical module that doesn't have a view, then pass `nil` (default).
+    ///
+    /// - Parameter builder: The builder that is responsible for creating child modules by their associated names.
+    /// Implement this by subclassing the `RABuilder` class.
+    /// If this module don't have child modules, then pass `nil` (default).
+    ///
+    public init(name: String, interactor: RAAbstractInteractor, router: RARouter, view: RAAbstractView? = nil, builder: RABuilder? = nil) {
+        defer { log("created", category: "ModuleLifecycle") }
         self.name = name
+        self.interactor = interactor
+        self.router = router
+        self.view = view
+        self.builder = builder
+        RALeakDetector.register(self)
+    }
+    
+    deinit {
+        log("deleted", category: "ModuleLifecycle")
+    }
+    
+}
+
+
+/// A module that is marked as empty. That is, it cannot be added to the module tree.
+final internal class RAEmptyModule: RAModule, RAEmpty {
+    
+    /// Creates an empty module.
+    internal init() {
+        super.init(name: "Empty", interactor: RAEmptyInteractor(), router: RAEmptyRouter())
     }
     
 }
