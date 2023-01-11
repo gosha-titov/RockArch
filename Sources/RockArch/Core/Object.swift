d011948de3989153a9897df398eecfb8c820f984
gosha <gosha.titov@icloud.com>
2023-01-11 15:50:04 +0300

    Feat: define the `RAWeakObject` class.

--- Sources/RockArch/Core/Object.swift
+++ Sources/RockArch/Core/Object.swift
@@ -1,3 +1,27 @@
+/// An object that weakly references a specific object.
+///
+/// Weak objects are mainly used in collections that should not strongly reference objects.
+open class RAWeakObject: RAObject {
+    
+    /// The reference to a specific object.
+    public weak var reference: RAObject?
+    
+    /// A string associated with the name of the reference object.
+    public let name: String
+    
+    /// A textual representation of the type of the referenc object.
+    public let type: String
+    
+    /// Creates a weak object that references a specific object.
+    public init(reference: RAObject) {
+        self.reference = reference
+        name = reference.name
+        type = reference.type
+    }
+    
+}
+
+
 /// A named reference-type object that has a description.
 public protocol RAObject: AnyObject, CustomStringConvertible {
     
