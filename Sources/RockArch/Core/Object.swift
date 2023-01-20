cf8a1f4a18c174275ada151aa6dce707f42aec2e
gosha <gosha.titov@icloud.com>
2023-01-21 00:40:29 +0300

    Feat: define the `RAWrappedObject` class.

--- Sources/RockArch/Core/Object.swift
+++ Sources/RockArch/Core/Object.swift
@@ -1,37 +1,53 @@
-/// The object that weakly references a specific object.
+/// The object that weakly references a specific `AnyObject` instance.
 ///
 /// Weak objects are mainly used in collections that should not strongly reference objects.
-open class RAWeakObject: RAObject {
+internal final class RAWeakObject: RAObject {
     
-    /// The reference to a specific object.
-    public weak var reference: RAObject?
+    /// The weak reference to a specific object.
+    internal weak var reference: AnyObject?
     
     /// A string associated with the name of the reference object.
-    public let name: String
+    internal let name: String
     
-    /// A textual representation of the type of the referenc object.
-    public let type: String
+    /// A textual representation of the type of the reference object.
+    internal let type: String
     
-    /// Creates a weak object that references a specific object.
-    public init(reference: RAObject) {
-        self.reference = reference
-        name = reference.name
-        type = reference.type
+    /// Creates a weak object that stores the given `RAObject` instance in the right way.
+    internal init(reference object: RAObject) {
+        if let weakObject = object as? RAWeakObject {
+            reference = weakObject.reference
+        } else if let wrappedObject = object as? RAWrappedObject {
+            reference = wrappedObject.value
+        } else {
+            reference = object
+        }
+        name = object.name
+        type = object.type
     }
     
 }
 
 
-/// The object that is ignored by others.
+/// The object that wraps a specific `AnyObject` instance by strongly referencing it.
 ///
-/// Empty objects are mainly used as an empty result of a function.
-internal final class RAEmptyObject: RAObject, RAEmpty {
+/// Wrapped objects are mainly used to endow the `AnyObject` instance with the behavior of a `RAObject`.
+internal final class RAWrappedObject: RAObject {
     
-    /// A string associated with the name of this empty object.
-    internal let name = "Empty"
+    /// The strong reference to a specific object.
+    internal let value: AnyObject
     
-    /// A textual representation of the type of this empty object.
-    internal let type = "Object"
+    /// A string associated with the name of the reference object.
+    internal let name: String
+    
+    /// A textual representation of the type of the reference object.
+    internal let type: String
+    
+    /// Creates a wrapped object that stores the given `AnyObject` instance.
+    internal init(name: String, type: String, objectToWrap: AnyObject) {
+        value = objectToWrap
+        self.name = name
+        self.type = type
+    }
     
 }
 
