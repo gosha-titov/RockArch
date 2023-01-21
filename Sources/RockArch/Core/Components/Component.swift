2826380342a865e675db5489db78b2acea005fb4
gosha <gosha.titov@icloud.com>
2023-01-21 18:31:57 +0300

    Feat: define the `RAComponentIntegratedIntoModule` protocol.

--- Sources/RockArch/Core/Components/Component.swift
+++ Sources/RockArch/Core/Components/Component.swift
@@ -1,8 +1,38 @@
+/// A component that is a part of a module.
+///
+/// The `RAInteractor`, `RARouter`, `RAPresenter` and `RABuilder` objects conforms to this protocol.
+/// These components have the same states and names as the modules to that they belong to.
+/// - Note: The `module` property should be weak to avoid memory leaks.
+public protocol RAComponentIntegratedIntoModule: RAComponent {
+    
+    /// A module to that this component belongs.
+    /*weak*/ var module: RAModule? { get set }
+    
+    /// Setups this component after the module is loaded into memory and assembled.
+    func setup()
+    
+    /// Cleans this component when the module is about to be unloaded from memory and disassembled.
+    func clean()
+    
+}
+
+public extension RAComponentIntegratedIntoModule {
+    
+    var state: RAComponentState {
+        return module?.state ?? .inactive
+    }
+    
+    var name: String {
+        return module?.name ?? "Unowned"
+    }
+    
+}
+
+
 /// A single responsibility component that is a part of the architecture.
 ///
-/// The `RAComponent` protocol is a basis of all key components, such as the `RAModule` objects,
-/// the `RAInteractor` objects, the `RARouter` objects, the `RAView` objects and the `RABuilder` objects.
-/// They constitute the backbone of the application architecture.
+/// The `RAComponent` protocol is a basis of all key components, such as the `RAModule`, `RAInteractor`, `RARouter`, `RAPresenter`,
+/// `RAView` and `RABuilder` objects. They constitute the backbone of the application architecture.
 public protocol RAComponent: RAObject, RASimplifiedLoggable {
     
     /// The current state of this component.
