94e2c5ed64d408f49f3cb8d6ab90ba78f3fc2ce1
gosha <gosha.titov@icloud.com>
2023-01-21 17:17:31 +0300

    Refactor: add the `RAView` property.

--- Sources/RockArch/Core/Components/Presenter.swift
+++ Sources/RockArch/Core/Components/Presenter.swift
@@ -26,8 +26,13 @@ open class RAAbstractPresenter: RAComponent {
     /// An internal interactor that is set by a module.
     internal weak var _interactor: RAAbstractInteractor?
     
+    /// The internal view of this presenter.
+    internal let _view: any RAView
+    
     /// The view controller of the module.
-    internal let viewController: UIViewController
+    internal final var viewController: UIViewController {
+        return _view
+    }
     
     /// The view controller as a navigation controller, or `nil`.
     internal final var navigationController: UINavigationController? {
@@ -52,39 +57,38 @@ open class RAAbstractPresenter: RAComponent {
     
     // MARK: - Lifecycle
     
-    /// Called when the module is loaded into memory.
-    internal final func _setup() -> Void {
-        let view = RAWrappedObject(name: name, type: "View", objectToWrap: viewController)
-        RALeakDetector.register(view)
-        setup()
-    }
-    
-    /// Called when the module is about to be unloaded from memory.
-    internal final func _clean() -> Void {
-        clean()
-    }
-    
     /// Setups this presenter.
     ///
-    /// This method is called when the module to which this presenter belongs is loaded into memory.
+    /// This method is called when the module to which this presenter belongs is loaded into memory and assembled.
     /// You usually override this method to perform additional initialization on your private properties.
     /// You don't need to call the `super` method.
     open func setup() -> Void {}
     
     /// Cleans this presenter.
     ///
-    /// This method is called when the module to which this presenter belongs is about to be unloaded from memory.
+    /// This method is called when the module to which this presenter belongs is about to be unloaded from memory and disassembled.
     /// You usually override this method to clean your properties.
     /// You don't need to call the `super` method.
     open func clean() -> Void {}
     
+    /// Called when the module is loaded into memory and assembled.
+    internal final func _setup() -> Void {
+        RALeakDetector.register(_view)
+        setup()
+    }
+    
+    /// Called when the module is about to be unloaded from memory and disassembled.
+    internal final func _clean() -> Void {
+        clean()
+    }
+    
     
     // MARK: - Internal Init
     
-    /// Creates an abstract presenter instance with a specific view controller.
-    internal init(viewController: UIViewController) {
-        self.viewController = viewController
-        viewController._presenter = self
+    /// Creates an abstract presenter instance with the given view.
+    internal init(view: any RAView) {
+        _view = view
+        _view._presenter = self
         RALeakDetector.register(self)
     }
     
