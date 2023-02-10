516e422b4ab9de4e07635f2f6ae4302c6c8bbe17
gosha <gosha.titov@icloud.com>
2023-02-10 15:31:05 +0300

    Feat: add the properties and methods for the tabs selecting.

--- Sources/RockArch/Core/Components/Router.swift
+++ Sources/RockArch/Core/Components/Router.swift
@@ -13,6 +13,9 @@ open class RARouter: RAComponentIntegratedIntoModule {
     /// The array of route actions that are mainly used by deep links.
     public private(set) var defaultTransitions = [String: RATransition]()
     
+    /// The array of names of child modules that are currently in the tab bar.
+    public private(set) var namesOfTabModules = [String]()
+    
     
     // MARK: Internal Properties
     
@@ -37,6 +40,12 @@ open class RARouter: RAComponentIntegratedIntoModule {
         let parent = module?.router(of: .parent)
         return parent?.viewController ?? parent?.previousViewController
     }
+    
+    
+    // MARK: Private Properties
+    
+    /// The array of names of child modules that should be added to the tab bar.
+    private var namesOfModulesThatShouldBeTabs = [String]()
     
     
     // MARK: - Routing
@@ -133,11 +142,77 @@ open class RARouter: RAComponentIntegratedIntoModule {
         return true
     }
     
+    /// Selects a view controller of a specific child module.
+    ///
+    /// You can present a child module only when this module has a tab bar controller.
+    /// When you do this, you start and display the child module.
+    ///
+    /// In order to specify tab modules, you should override the `setup()` method of the module to which this router belongs
+    /// and mark some modules as integrated:
+    ///
+    ///     final class MainModule: RAModule {
+    ///
+    ///         override func setup() -> Void {
+    ///             markModuleAsIntegrated(byName: "Feed")
+    ///             markModuleAsIntegrated(byName: "Messages")
+    ///             markModuleAsIntegrated(byName: "Settings")
+    ///         }
+    ///
+    ///     }
+    ///
+    /// And then mark specific modules from them as tabs:
+    ///
+    ///     final class MainRouter: RARouter {
+    ///
+    ///         override func setup() -> Void {
+    ///             markIntegratedModuleAsTab(byName: "Feed")
+    ///             markIntegratedModuleAsTab(byName: "Messages")
+    ///             markIntegratedModuleAsTab(byName: "Settings")
+    ///         }
+    ///
+    ///     }
+    ///
+    /// - Parameter childName: The name of a module to start and select.
+    /// - Parameter completion: The block to execute after the selecting finishes.
+    /// This block has no return value and takes no parameters. The default value is `true.`
+    ///
+    /// - Returns: `True` if the child module has been selected; otherwise, `false`.
+    @discardableResult
+    public final func selectChildModule(byName childName: String, completion: (() -> Void)? = nil) -> Bool {
+        guard let module else {
+            log("Couldn't select the \(childName) child module because this router doesn't belong to any module",
+                category: .moduleRouting,
+                level: .error)
+            return false
+        }
+        guard let tabBarController else {
+            log("Couldn't select the \(childName) child module because this router doesn't have a tab bar controller",
+                category: .moduleRouting,
+                level: .error)
+            return false
+        }
+        guard let tabIndex = namesOfTabModules.firstIndex(of: childName) else {
+            log("Couldn't select the \(childName) child module because it's not a tab",
+                category: .moduleRouting,
+                level: .error)
+            return false
+        }
+        guard module.invokeChildModule(byName: childName) else {
+            log("Couldn't select the \(childName) child module because it cannot be invoked",
+                category: .moduleRouting,
+                level: .error)
+            return false
+        }
+        tabBarController.selectedIndex = tabIndex
+        completion?()
+        return true
+    }
+    
     
     // MARK: - Transition Management
     
-    /// Adds the given transition for a specific child.
-    public final func addDefaultTransition(_ transition: RATransition, forChild childName: String) -> Void {
+    /// Sets the given transition for a specific child.
+    public final func setDefaultTransition(_ transition: RATransition, forChild childName: String) -> Void {
         if defaultTransitions[childName].hasValue {
             log("Replaced an existing transition for the `\(childName)` child module",
                 category: .moduleRouting,
@@ -155,6 +230,90 @@ open class RARouter: RAComponentIntegratedIntoModule {
         } else {
             defaultTransitions.removeValue(forKey: childName)
         }
+    }
+    
+    
+    // MARK: - Tab Bar Setuping
+    
+    /// Marks a specific integrated module as a tab.
+    ///
+    /// You use it when the module has a tab bar controller:
+    ///
+    ///     override func setup() -> Void {
+    ///         markIntegratedModuleAsTab(byName: "Feed")
+    ///         markIntegratedModuleAsTab(byName: "Messages")
+    ///         markIntegratedModuleAsTab(byName: "Settings")
+    ///     }
+    ///
+    /// Be sure, that you mark them for the module to which this router belongs:
+    ///
+    ///     final class MainModule: RAModule {
+    ///
+    ///         override func setup() -> Void {
+    ///             markModuleAsIntegrated(byName: "Feed")
+    ///             markModuleAsIntegrated(byName: "Messages")
+    ///             markModuleAsIntegrated(byName: "Settings")
+    ///         }
+    ///
+    ///     }
+    ///
+    public final func markIntegratedModuleAsTab(byName childName: String) -> Void {
+        guard let module else {
+            log("Couldn't mark the `\(childName)` child module as a tab because this router didn't belong to any module",
+                category: .moduleRouting,
+                level: .error)
+            return
+        }
+        guard tabBarController.hasValue else {
+            log("Couldn't mark the `\(childName)` child module as a tab because the module didn't have a tab bar controller",
+                category: .moduleRouting,
+                level: .error)
+            return
+        }
+        guard module.namesOfIntegratedModules.contains(childName) else {
+            log("Couldn't mark the `\(childName)` child module as a tab because the module didn't integrate this module",
+                category: .moduleRouting,
+                level: .error)
+            return
+        }
+        guard namesOfModulesThatShouldBeTabs.contains(childName) == false else {
+            log("Couldn't mark the `\(childName)` child module as a tab because it was already marked",
+                category: .moduleRouting,
+                level: .error)
+            return
+        }
+        namesOfModulesThatShouldBeTabs.append(childName)
+    }
+    
+    /// Setups a tab bar controller if the module has it.
+    private func setupTabBarControllerIfNeeded() -> Void {
+        guard let module, let tabBarController else { return }
+        guard module.isLoaded == false else {
+            log("Couldn't setup a tab bar controller because the module was already loaded",
+                category: .moduleRouting,
+                level: .error)
+            return
+        }
+        guard module.namesOfIntegratedModules.count > 0 else {
+            log("Couldn't setup a tab bar controller because integrated modules weren't specified",
+                category: .moduleRouting,
+                level: .error)
+            return
+        }
+        let childNames = namesOfModulesThatShouldBeTabs
+        var childViewControllers = [UIViewController]()
+        for childName in childNames {
+            if let child = module.router(of: .child(childName)),
+               let childViewController = child.viewController {
+                childViewControllers.append(childViewController)
+                namesOfTabModules.append(childName)
+            } else {
+                log("Couldn't add the `\(childName)` child module to a tab bar because it didn't have a view controller",
+                    category: .moduleRouting,
+                    level: .error)
+            }
+        }
+        tabBarController.setViewControllers(childViewControllers, animated: false)
     }
     
     
@@ -176,8 +335,9 @@ open class RARouter: RAComponentIntegratedIntoModule {
     
     /// Called when the module is loaded into memory and assembled.
     internal final func _setup() -> Void {
-        defer { setup() }
         RALeakDetector.register(self)
+        setup()
+        setupTabBarControllerIfNeeded()
     }
     
     /// Called when the module is about to be unloaded from memory and disassembled.
