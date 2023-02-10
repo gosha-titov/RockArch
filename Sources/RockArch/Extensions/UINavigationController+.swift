9e3e31996758261a85f3a759558a263c5b3aab02
gosha <gosha.titov@icloud.com>
2023-02-10 15:35:22 +0300

    Refactor: make the `completion` argument optional.

--- Sources/RockArch/Extensions/UINavigationController+.swift
+++ Sources/RockArch/Extensions/UINavigationController+.swift
@@ -3,7 +3,7 @@ import UIKit
 internal extension UINavigationController {
     
     /// Pushes a view controller onto the receiver’s stack and updates the display.
-    func push(_ viewController: UIViewController, animated: Bool = true, completion: @escaping () -> Void) -> Void {
+    func push(_ viewController: UIViewController, animated: Bool = true, completion: (() -> Void)?) -> Void {
         CATransaction.begin()
         CATransaction.setCompletionBlock(completion)
         pushViewController(viewController, animated: animated)
@@ -11,7 +11,7 @@ internal extension UINavigationController {
     }
     
     /// Pops the top view controller from the navigation stack and updates the display.
-    func popViewController(animated: Bool = true, completion: @escaping () -> Void) -> UIViewController? {
+    func popViewController(animated: Bool = true, completion: (() -> Void)?) -> UIViewController? {
         CATransaction.begin()
         CATransaction.setCompletionBlock(completion)
         let viewController = popViewController(animated: animated)
@@ -20,7 +20,7 @@ internal extension UINavigationController {
     }
     
     /// Pops all the view controllers on the stack except the root view controller and updates the display.
-    func popToRootViewController(animated: Bool = true, completion: @escaping () -> Void) -> [UIViewController]? {
+    func popToRootViewController(animated: Bool = true, completion: (() -> Void)?) -> [UIViewController]? {
         CATransaction.begin()
         CATransaction.setCompletionBlock(completion)
         let viewControllers = popToRootViewController(animated: animated)
