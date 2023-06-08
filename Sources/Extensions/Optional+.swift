9c39fb4f43c443513e494e718df62203fb14e387
gosha <gosha.titov@icloud.com>
2023-06-08 20:51:14 +0300

    Feat: add the `isNil`, `hasValue` and `isNilOrEmpty` properties

---
+++ Sources/Extensions/Optional+.swift
@@ -0,0 +1,37 @@
+internal extension Optional {
+    
+    /// A boolean value that indicates whether the optional object is `nil`.
+    ///
+    ///     var age: Int?
+    ///     age.isNil // true
+    ///
+    var isNil: Bool { self == nil }
+    
+    /// A boolean value that indicates whether the optional object is not `nil`.
+    ///
+    ///     var name: String? = "gosha"
+    ///     name.hasValue // true
+    ///
+    var hasValue: Bool { self != nil }
+    
+}
+
+
+internal extension Optional where Wrapped: Collection {
+    
+    /// A boolean value that indicates whether the optional object is `nil` or empty.
+    ///
+    ///     var str: String? = nil
+    ///     str.isNilOrEmpty // true
+    ///
+    ///     var array: [Int]? = []
+    ///     array.isNilOrEmpty // true
+    ///
+    ///     var dict: [Int: String]? = [12: "34"]
+    ///     dict.isNilOrEmpty // false
+    ///
+    var isNilOrEmpty: Bool {
+        return self?.isEmpty ?? true
+    }
+    
+}
