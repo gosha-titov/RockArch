4b8d8b79a89e6a863c7daceed7e862ccca2ae994
gosha <gosha.titov@icloud.com>
2023-06-08 19:36:25 +0300

    Initial Commit

---
+++ Sources/RockArch/RockArch.swift
@@ -0,0 +1,6 @@
+public struct RockArch {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/RockArchTests/RockArchTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import RockArch
+
+final class RockArchTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(RockArch().text, "Hello, World!")
+    }
+}
