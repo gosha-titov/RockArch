3618173490dc91d39ef350ac2972d19179087504
gosha <gosha.titov@icloud.com>
2023-06-08 19:50:33 +0300

    Chore: update the `Package` file

--- Package.swift
+++ Package.swift
@@ -5,24 +5,26 @@ import PackageDescription
 
 let package = Package(
     name: "RockArch",
+    platforms: [
+        .iOS(.v12)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "RockArch",
-            targets: ["RockArch"]),
+            targets: ["RockArch"]
+        ),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RockArch",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"
+        ),
         .testTarget(
             name: "RockArchTests",
-            dependencies: ["RockArch"]),
-    ]
+            dependencies: ["RockArch"]
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
 )
