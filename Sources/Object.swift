e93e4ebc3a7a8c9b545e55669fa0354ab9e2ba06
gosha <gosha.titov@icloud.com>
2023-06-08 20:53:42 +0300

    Feat: define the `RAWeakObject` struct

--- Sources/Object.swift
+++ Sources/Object.swift
@@ -1,3 +1,45 @@
+/// The object that weakly references a specific `AnyObject` instance.
+///
+/// Weak objects are mainly used in collections that should not strongly reference objects.
+public struct RAWeakObject: RAObject {
+    
+    /// The weak reference a specific object.
+    public private(set) weak var reference: AnyObject?
+    
+    /// A string associated with the name of the reference object.
+    public let name: String
+    
+    /// A textual representation of the type of the reference object.
+    public let type: String
+    
+    /// Creates an object that weakly references the given `RAAnyObject` instance and has the same name and type as it.
+    ///
+    /// For example:
+    ///
+    ///     object.name // "Main"
+    ///     object.type // "Module"
+    ///
+    ///     let weakObject = RAWeakObject(reflecting: object)
+    ///     weakObject.name // "Main"
+    ///     weakObject.type // "Module"
+    ///
+    public init(reflecting object: RAAnyObject) {
+        reference = object
+        name = object.name
+        type = object.type
+    }
+    
+    /// Creates an object that weakly references the given `AnyObject` instance.
+    public init(name: String? = nil, type: String? = nil, referencing object: AnyObject) {
+        self.name = name.isNilOrEmpty ? "Unnamed"   : name!
+        self.type = type.isNilOrEmpty ? "AnyObject" : type!
+        reference = object
+    }
+    
+}
+
+
+
 /// A named typed reference-type object that has a description.
 ///
 /// Classes that conform to the `RAAnyObject` protocol can be identified by the `name` and `type` properties:
