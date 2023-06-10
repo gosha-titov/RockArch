beb5beaec11a879f9d0d2426de57d955b18f8e22
gosha <gosha.titov@icloud.com>
2023-06-10 16:37:00 +0300

    Feat: add `loggers` and `queue` properties, update the default initializer

--- Sources/Logging/BlackBox.swift
+++ Sources/Logging/BlackBox.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 /// The black box that records all incoming messages by passing them to its loggers.
 open class RABlackBox: RAAnyObject {
     
@@ -9,8 +11,19 @@ open class RABlackBox: RAAnyObject {
     /// This property has the "BlackBox" value.
     public let type = "BlackBox"
     
-    /// Creates a black box instance.
-    public init(name: String) {
+    /// Loggers that receive all messages from this black box.
+    public let loggers: [RALogger]
+    
+    /// The queue in which logging performs.
+    public let queue: DispatchQueue
+    
+    /// Creates a black box instance with specific loggers that will log messages in the given queue.
+    /// - Parameter name: The string associated with the name of this black box.
+    /// - Parameter loggers: Loggers that will receive all messages coming into this black box.
+    /// - Parameter queue: The queue in which logging will be performed. To keep the correct order of messages, pass a serial queue.
+    public init(name: String, loggers: [RALogger], queue: DispatchQueue) {
+        self.loggers = loggers
+        self.queue = queue
         self.name = name
     }
     
