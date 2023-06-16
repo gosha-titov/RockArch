48d62cf79ac9ffc2f9e94e5b0e72319d4e387115
gosha <gosha.titov@icloud.com>
2023-06-17 01:13:46 +0300

    Feat: add inner components, update init and deinit

--- Sources/Components/Module.swift
+++ Sources/Components/Module.swift
@@ -1,5 +1,7 @@
 open class RAModule: RAComponent {
     
+    // MARK: General Info
+    
     /// A string associated with the name of this module.
     public let name: String
     
@@ -11,23 +13,75 @@ open class RAModule: RAComponent {
     /// The current state of this module.
     public private(set) var state: RAComponentState = .inactive
     
-    /// Setups this module.
+    
+    // MARK: Inner Components
+    
+    /// The internal interactor of this module.
+    internal let interactor: RAInteractor
+    
+    /// The internal router of this module.
+    internal let router: RARouter
+    
+    /// The internal view of this module, or `nil`.
+    internal let view: (any RAView)?
+    
+    /// The internal builder of this module, or `nil`.
+    internal let builder: RABuilder?
+    
+    
+    // MARK: - Lifecycle
+    
+    /// Setups this module before it starts working.
     ///
     /// This method is called when this module is loaded into memory and assembled.
     /// You usually override this method to perform additional initialization on your private properties.
     /// You don't need to call the `super` method.
     open func setup() {}
     
-    /// Cleans this module.
+    /// Cleans this module after it stops working.
     ///
     /// This method is called when this module is about to be unloaded from memory and disassembled.
     /// You usually override this method to clean your properties.
     /// You don't need to call the `super` method.
     open func clean() {}
     
-    /// Creates a named module instance.
-    public init(name: String) {
+    
+    // MARK: - Init and Deinit
+    
+    /// Creates a named module instance with the given components.
+    ///
+    /// - Parameter name: A string that will be associated with this module.
+    /// The inner components (interactor, router, view, builder) of this module will also have this name.
+    /// It will be displayed in logs, paths, links, etc.
+    ///
+    /// - Parameter interactor: The interactor that is responsible for all business logic of this module.
+    /// It's also a built-in delegate of the module lifecycle and It can interact with other related interactors.
+    /// Implement this by subclassing the `RAInteractor` class.
+    ///
+    /// - Parameter router: The router that is responsible for the hierarchy of modules.
+    /// It can show and hide child modules, and it can complete this module.
+    /// Implement this by subclassing the `RARouter` class.
+    ///
+    /// - Parameter view: The view that is responsible for configurating and updating UI, catching and handling user interactions.
+    /// Implement this by creating a new class that conforms to the `RAView` protocol.
+    /// If this is a logical module that doesn't have a view, then pass `nil` (default).
+    ///
+    /// - Parameter builder: The builder that is responsible for creating child modules by their associated names.
+    /// Implement this by subclassing the `RABuilder` class.
+    /// If this module don't have child modules, then pass `nil` (default).
+    ///
+    public init(name: String, interactor: RAInteractor, router: RARouter, view: (any RAView)? = nil, builder: RABuilder? = nil) {
+        defer { log("Created", category: "ModuleLifecycle") }
         self.name = name
+        self.interactor = interactor
+        self.router = router
+        self.view = view
+        self.builder = builder
+        RALeakDetector.register(self)
+    }
+    
+    deinit {
+        log("Deleted", category: "ModuleLifecycle")
     }
     
 }
