576a8725a1ff5bce016fa58a31d3e474ca06a368
gosha <gosha.titov@icloud.com>
2023-06-17 21:57:22 +0300

    Feat: define the `RAContext` typealias

--- Sources/Components/Core/Data.swift
+++ Sources/Components/Core/Data.swift
@@ -3,3 +3,14 @@
 /// When one module loads another, it can pass a dependency that should be injected.
 /// It's usually a single service or multiple ones.
 public typealias RADependency = Any
+
+
+
+/// A context within which a module starts the work.
+///
+/// When one module shows another, it can pass a context in order to indicate key information.
+///
+/// For example, you have a table with friends, when a user clicks on a row, you show a friend profile.
+/// So, you have the `FriendsModule` and `FriendProfileModule` classes.
+/// That is, the context for the second module is a `friendID` that you pass from the first module.
+public typealias RAContext = Any
