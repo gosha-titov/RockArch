00c975b332657cc544ac12d2fcdfbd2ee54eace8
gosha <gosha.titov@icloud.com>
2023-06-19 01:14:19 +0300

    Feat: add the `removeDuplicates()` method

--- Sources/Extensions/Array+.swift
+++ Sources/Extensions/Array+.swift
@@ -1,14 +1,23 @@
 internal extension Array where Element: Equatable {
     
-    /// Returns an array containing all but duplicates.
-    ///
-    ///     let array = [1, 2, 2, 4, 5, 5, 5]
-    ///     array.removedDuplicates() // [1, 2, 4, 5]
-    ///
-    func removedDuplicates() -> [Element] {
-        var result = [Element]()
-        forEach { if !result.contains($0) { result.append($0) } }
-        return result
-    }
+        /// Returns an array containing all but duplicates, leaving only the first element of them.
+        ///
+        ///     let array = [1, 2, 3, 2, 4, 4, 5, 4]
+        ///     array.removedDuplicates() // [1, 2, 3, 4, 5]
+        ///
+        func removedDuplicates() -> [Element] {
+            var result = [Element]()
+            forEach { if !result.contains($0) { result.append($0) } }
+            return result
+        }
+        
+        /// Removes all duplicate elements, leaving only the first element of them.
+        ///
+        ///     var array = [1, 2, 3, 2, 4, 4, 5, 4]
+        ///     array.removeDuplicates() // [1, 2, 3, 4, 5]
+        ///
+        mutating func removeDuplicates() -> Void {
+            self = removedDuplicates()
+        }
     
 }
