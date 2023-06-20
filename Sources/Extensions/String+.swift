2f895336c4c04802efba1cb31a55d4d77d60f4db
gosha <gosha.titov@icloud.com>
2023-06-20 16:44:44 +0300

    Feat: add static properties corresponding with log categories

---
+++ Sources/Extensions/String+.swift
@@ -0,0 +1,9 @@
+internal extension String {
+    
+    /// The log category that has the "ModuleManagement" value.
+    static var moduleManagement = "ModuleManagement"
+    
+    /// The log category that has the "ModuleLifecycle" value.
+    static var moduleLifecycle = "ModuleLifecycle"
+    
+}
