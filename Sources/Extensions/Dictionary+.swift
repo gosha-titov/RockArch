3264aaa73c259d9eed75b6e4c14610d1721c4aad
gosha <gosha.titov@icloud.com>
2023-07-16 17:48:43 +0300

    Feat: add the `hasKey(_:)` method

--- Sources/Extensions/Dictionary+.swift
+++ Sources/Extensions/Dictionary+.swift
@@ -1,3 +1,16 @@
+internal extension Dictionary {
+    
+    /// Returns a boolean value that indicates whether the given key exists in this dictionary.
+    ///
+    ///     let dict = [0: "a", 1: "b", 2: "c"]
+    ///     dict.hasKey(2) // true
+    ///     dict.hasKey(3) // false
+    ///
+    func hasKey(_ key: Key) -> Bool { self[key] != nil }
+    
+}
+
+
 internal extension Dictionary.Keys {
     
     /// Returns an array consisting of keys.
