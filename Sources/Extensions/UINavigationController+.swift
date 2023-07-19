70d0769f97ca713b83cabc0ab5e757fa50fe2828
gosha <gosha.titov@icloud.com>
2023-07-19 18:49:34 +0300

    Feat: add methods with the `completion` parameter

---
+++ Sources/Extensions/UINavigationController+.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+internal extension UINavigationController {
+    
+    /// Pushes a view controller onto the receiver’s stack and updates the display.
+    func push(_ viewController: UIViewController, animated: Bool = true, completion: (() -> Void)?) -> Void {
+        CATransaction.begin()
+        CATransaction.setCompletionBlock(completion)
+        pushViewController(viewController, animated: animated)
+        CATransaction.commit()
+    }
+    
+    /// Pops the top view controller from the navigation stack and updates the display.
+    func popViewController(animated: Bool = true, completion: (() -> Void)?) -> UIViewController? {
+        CATransaction.begin()
+        CATransaction.setCompletionBlock(completion)
+        let viewController = popViewController(animated: animated)
+        CATransaction.commit()
+        return viewController
+    }
+    
+    /// Pops all the view controllers on the stack except the root view controller and updates the display.
+    func popToRootViewController(animated: Bool = true, completion: (() -> Void)?) -> [UIViewController]? {
+        CATransaction.begin()
+        CATransaction.setCompletionBlock(completion)
+        let viewControllers = popToRootViewController(animated: animated)
+        CATransaction.commit()
+        return viewControllers
+    }
+    
+}
