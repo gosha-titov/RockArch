ee40e8e7ec1c24692ad92510f2123b051cd3155c
gosha <gosha.titov@icloud.com>
2023-07-25 04:21:45 +0300

    Feat: add a new log category

--- Sources/Extensions/String+.swift
+++ Sources/Extensions/String+.swift
@@ -3,6 +3,9 @@ internal extension String {
     /// The log category that has the "ModuleCommunication" value.
     static let moduleCommunication = "ModuleCommunication"
     
+    /// The log category that has the "ModuleInteracting" value.
+    static let moduleInteracting = "ModuleInteracting"
+    
     /// The log category that has the "ModuleManagement" value.
     static let moduleManagement = "ModuleManagement"
     
